4539188136a8962e53352319ef66663025c4efa0
boxy8 <zou.emily@yahoo.co.nz>
2022-07-12 23:42:59 +1200

    Clean up

--- pages/login.js
+++ pages/login.js
@@ -7,29 +7,25 @@ function Login({ providers }) {
             
             {Object.values(providers).map((provider) => (
                 <div key={provider.name}>
-                    <button onClick={() => signIn(provider.id, { callbackUrl: "/" }
-                        )}
-                    >
+                    <button onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
                         Login with {provider.name}
                     </button>
                 </div>
             ))
             }
-
-            
         </div>
-    )
+    );
 }
 
 // This gets called on every request
 export async function getServerSideProps() {
-    const providers = await getProviders()
+    const providers = await getProviders();
   
     // Pass data to the page via props
-    return { props: { providers }}
+    return { props: { providers }};
   }
   
-  export default Login
+  export default Login;
 
 
 
