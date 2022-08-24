9bb2fd88b76539340e7954b154029a07e76d1ce3
boxy8 <zou.emily@yahoo.co.nz>
2022-08-25 00:39:22 +1200

    feat: Implement switch between work mode and break mode on timer end

--- components/MusicTimer.jsx
+++ components/MusicTimer.jsx
@@ -1,21 +1,95 @@
 import useSpotify from '../hooks/useSpotify';
-import { useTimer } from '../hooks/useTimer';
-import { useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
 import Timer from '../components/Timer';
 
 function MusicTimer() {
+    // unit: minutes
+    const duration = {
+        'work': 1,
+        'break': 1
+    };
     const spotifyApi = useSpotify();
-    const [isActive, setIsActive] = useState(false);   
+    const [isRunning, setIsRunning] = useState(false);
+    const [timerValue, setTimerValue] = useState(duration.work * 60 * 1000);
+    const [mode, setMode] = useState('work'); // work or break mode
+    const [endTimestamp, setEndTimestamp] = useState(duration.work * 60 * 1000 + Date.now());  
+    const timeout = useRef();
 
-    useEffect(() => {
-        toggleMusic(spotifyApi);    // k problem is there is now noticeable delay...
-    }, [isActive])
+    function resume() {
+        setIsRunning(true)
+        setEndTimestamp(timerValue + Date.now());
+        syncMusic('play');
+    }
+
+    function pause() {
+        clearTimeout(timeout);
+        setIsRunning(false);
+        syncMusic('pause');
+    }
+
+    function switchMode() {
+        pause();
+        const nextMode = mode === 'work' ? 'break' : 'work';
+        const nextTimerValue = (nextMode === 'work' ? duration.work : duration.break) * 60 * 1000;
+
+        setMode(nextMode);
+        setTimerValue(nextTimerValue);
+    }
+
+    function syncMusic(timerStatus) {
+        spotifyApi.getMyCurrentPlaybackState()
+            .then(function (data) {
+                if (data.body && data.body.is_playing) {
+                    if (timerStatus == 'pause') {
+                        spotifyApi.pause()
+                        .then(function () {
+                            console.log('Playback paused');
+                        }, function (err) {
+                            //if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
+                            console.log('Something went wrong!', err);
+                        });
+                    }
+                } else {
+                    if (timerStatus == 'play') {
+                        spotifyApi.play()
+                        .then(function () {
+                            console.log('Playback started');
+                        }, function (err) {
+                            //if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
+                            console.log('Something went wrong!', err);
+                        });
+                    }
+                }
+            });
+    }
+
+    if (isRunning) {
+        timeout.current = setTimeout(() => {
+            if (endTimestamp - Date.now() <= 0) {
+                switchMode();
+            } else {
+                setTimerValue(endTimestamp - Date.now());
+            }
+        }, 500);
+    }
+
+    const [hrs, mins, secs] = getReturnValues(timerValue);
+
+    function getReturnValues(timerValue) {
+        // calculate timer value in terms of hrs, mins, secs
+        const hours = Math.floor(timerValue / (1000 * 60 * 60));
+        const minutes = Math.floor((timerValue % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timerValue % (1000 * 60)) / 1000);
+    
+        return [hours, minutes, seconds];
+    };
 
     return (
         <>
             <div className="container">
                 <div className="main-wrapper">
-                    <Timer isActive={isActive} duration={1} setIsActive={setIsActive}/>
+                <h1>{mode}</h1>
+                <div>{hrs} : {mins} : {secs}</div>
                     <div className="now-playing__side">
 
                         <button className="btn-spotify" onClick={() => { 
@@ -31,13 +105,10 @@ function MusicTimer() {
                             &lt;&lt;
                         </button>
 
-                        <button className="btn-spotify" onClick={() => {
-                            // remember please that it is not being called on render... it calls on click...
-                            // logic to toggle isActive. music playback being toggled after render.
-                            setIsActive(prevIsActive => !prevIsActive);
-                         }} >
-                            { isActive ? "PAUSE" : "PLAY" }
-                        </button>
+                         {isRunning 
+                            ? <button className="btn-spotify" onClick={pause}>PAUSE</button>
+                            : <button className="btn-spotify" onClick={resume}>PLAY</button>
+                         }
 
                         <button className="btn-spotify" onClick={() => { 
                             spotifyApi.skipToNext()
@@ -55,35 +126,6 @@ function MusicTimer() {
             </div>
         </>
     );
-}
-
-function toggleMusic(spotifyApi) {
-        // check if currently play or pause
-        spotifyApi.getMyCurrentPlaybackState()
-            .then(function (data) {
-                // Output items
-                if (data.body && data.body.is_playing) {
-                    // we want to pause
-                    spotifyApi.pause()
-                        .then(function () {
-                            console.log('Playback paused');
-                        }, function (err) {
-                            //if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-                            console.log('Something went wrong!', err);
-                        });
-                } else {
-                    // we want to play
-                    spotifyApi.play()
-                        .then(function () {
-                            console.log('Playback started');
-                        }, function (err) {
-                            //if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-                            console.log('Something went wrong!', err);
-                        });
-                }
-            }, function (err) {
-                console.log('Something went wrong!', err);
-            });
 }
 
 export default MusicTimer;
